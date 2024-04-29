8ddf508f3bd4cbeaeaea83405087dc48be947cd6
L-Hands <lxh897@student.bham.ac.uk>
2024-04-29 15:59:29 +0100

    Added a scalar state mutliplacation method to the unittest class

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,26 +2,17 @@ import numpy as np
 import diracpy as dp
 
 
-#Initialising Fock Space for Tests
-pump = dp.floquet_subspace(index = 0)
-cav = dp.fock_subspace(index = 1)
-atom = dp.two_level_subspace(index = 2)
-
-
 class unittests:
     def __init__(self):
         self.pump = dp.floquet_subspace(index = 0)
         self.cav = dp.fock_subspace(index = 1)
         self.atom = dp.two_level_subspace(index = 2)
-        self.test_ground_ket = dp.ket([0,1,'g'])
-        self.test_excited_ket = dp.ket([0,0,'e'])
+        self.example_ground_ket = dp.ket([0,1,'g'])
+        self.example_excited_ket = dp.ket([0,0,'e'])
     
-    def scalar_multiplacation_test(self):
-        
-        bool1 = True
-        bool2 = False    
-        testarray = [["test_item1", bool1],["test_item2", bool2]] #This can be tidied
-        
+    def state_scalar_multiplacation_test(self):
+        test_op = self.example_ground_ket
+        new_op = np.sqrt(2) * self.cav.a  
         
         all_true = bool1 + bool2
         if all_true == True:
@@ -30,6 +21,11 @@ class unittests:
             pass
         else:
             return testarray
+
+
+
+
+
 def blank_test():
     
     bool1 = True
