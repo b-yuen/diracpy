17924a377ba6d6a84254a51ccb115251e9a2aa2a
L-Hands <lxh897@student.bham.ac.uk>
2024-04-29 16:47:30 +0100

    Added string test and started empty ket test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,7 +2,7 @@ import numpy as np
 import diracpy as dp
 
 
-class unittests:
+class unittest_base:
     def __init__(self):
         self.pump = dp.floquet_subspace(index = 0)
         self.cav = dp.fock_subspace(index = 1)
@@ -10,9 +10,34 @@ class unittests:
         self.example_ground_ket = dp.ket([0,1,'g'])
         self.example_excited_ket = dp.ket([0,0,'e'])
     
+    def string_test(self):
+        psi0 = self.example_ground_ket
+        psi0_str = str(psi0)
+        print(psi0_str)
+    
+    
+class unittest_states(unittest_base):
+    def string_test(self):
+        psi0 = self.example_ground_ket
+        psi0_str = str(psi0)
+        print(psi0_str)
+    
+    def empty_action_ket(self):
+        atom = self.atom
+        psi0 = dp.ket()
+        psi_m = atom.sigma_minus * psi0
+        psi_p = atom.sigma_plus * psi0
+        psi_mp = atom.sigma_minus * atom.sigma_plus * psi0
+        psi_pm = atom.sigma_plus * atom.sigma_minus * psi0
+        ##psi1 = cav.a * psi0
+        #print('psi0 = ', end='')
+        #psi0.print()
+        #print('psi1 = ', end='')
+        #psi1.print()
+    
     def state_scalar_multiplacation_test(self):
-        test_op = self.example_ground_ket
-        new_op = np.sqrt(2) * self.cav.a  
+        test_state = self.example_ground_ket
+        new_state = np.sqrt(2) * self.cav.a  
         
         all_true = bool1 + bool2
         if all_true == True:
@@ -24,7 +49,8 @@ class unittests:
 
 
 
-
+class unittest_qop(unittest_base):
+    
 
 def blank_test():
     
