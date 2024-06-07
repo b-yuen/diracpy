3eccd9508dc060451217a44fd83190adede50c54
b-yuen <benyuen@hotmail.co.uk>
2024-06-07 11:33:18 +0100

    Changed sphinx documentation theme to the default 'alabaster'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,11 +41,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 # html_theme = 'furo'
 # html_theme = 'piccolo_theme'
 html_permalinks_icon = '§'
-html_theme = 'insipid'
+# html_theme = 'insipid'
 html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
